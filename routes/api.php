6bc6999619f05ff9bb11d841e592f41fbd91d473
ChromeBridge321 <davidgarcia2809@gmail.com>
2025-03-19 11:44:12 -0600

    .

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::prefix(('v1'))->middleware(['api','jwt.verify'])->group(function(){
         Route::put('/{id}',[ProductController::class,'update']);
         Route::delete('/{id}',[ProductController::class,'destroy']);
         Route::get('/search/searchByName',[ProductController::class,'searchByName']);
-        Route::get('/purgar', [OrderController::class,'purgar']);
+        Route::get('/purgar', [ProductController::class,'purgar']);
     });   
 });
 
